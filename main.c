77034f4603b250f34c4243b61a8850f14625cdc1
dimka90 <dimkayilrit@gmail.com>
2023-03-04 23:33:32 +0100

    Odin Library project using c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+void Library(char title[],char author[],int pages, bool read){
+  printf("Book tittle: %s",title);
+  printf("\nBook authoor: %s",author);
+  printf("\nBook pages: %d",pages);
+  if(read){
+  printf("\nRead Status:Finished reading");
+    }
+else{
+  printf("\nBook Completion:Not yet..!!!");
+    }
+}
+
+
+int main()
+{
+Library("The Lion king","Dimka",20,false);
+      printf("\n");
+
+    return 0;
+  
+} 
